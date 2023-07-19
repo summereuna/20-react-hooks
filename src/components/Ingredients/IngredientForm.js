44eef8350fafa14c850f11872825f8a1e0b72b4e
summereuna <summereuna@gmail.com>
2023-07-20 00:06:41 +0900

    Add title and amount state

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
+const IngredientForm = React.memo((props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+
+  const titleChangeHandler = (event) => {
     event.preventDefault();
-    // ...
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    event.preventDefault();
+    setEnteredAmount(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    //console.log(event.target.value);
+    // setInputState({})
   };
 
   return (
@@ -15,11 +29,21 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={enteredTitle}
+              onChange={titleChangeHandler}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={enteredAmount}
+              onChange={amountChangeHandler}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <Provider store={store}>
-//         <BrowserRouter>
-//             <App />
-//         </BrowserRouter>
-//     </Provider>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  //<Provider store={store}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  //</Provider>
+);
