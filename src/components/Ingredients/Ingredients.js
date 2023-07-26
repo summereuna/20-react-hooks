c0350d93a706d4c89bea39db901f0adc07d1b3fc
summereuna <summereuna@gmail.com>
2023-07-26 14:54:58 +0900

    Add data fetch and search fn

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
@@ -8,6 +8,44 @@ const Ingredients = () => {
   //여기서 Form에서 인풋 받아서 리스트로 출력함
   //여기서 재료를 관리한다는건 useState()를 사용해야 한다는 뜻
   const [userIngredients, setUserIngredients] = useState([]);
+
+  //Ingredients 컴포넌트 렌더링 될 때 마다 모든 재료 목록 가져와야 함
+  //컴포넌트가 마운트 될 때 데이터 가져오기
+  //useEffect() 사용
+  useEffect(() => {
+    //Ingredients 컴포넌트가 렌더링된 이후 실행
+    // 그리고 Ingredients 컴포넌트가 렌더링 될 때 마다 실행되는 함수
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://react-http-35c4a-default-rtdb.firebaseio.com/ingredients.json"
+      );
+
+      const resData = await response.json();
+      const loadedIngredients = [];
+      for (const key in resData) {
+        //loadedIngredients는 상수이지만, push()sms loadedIngredients에 저장된 값을 변경하는게 아닌 메모리에 있는 배열을 변경하는 거기 때문에 사용 가능
+        //세로운 객체 빈 배열인 loadedIngredients에 넣기
+        loadedIngredients.push({
+          id: key,
+          title: resData[key].title,
+          amount: resData[key].amount,
+        });
+      }
+
+      setUserIngredients(loadedIngredients);
+    };
+
+    fetchData();
+  }, []);
+
+  // useEffect(() => {
+  //   console.log("재료 목록 렌더링: ", userIngredients);
+  // }, [userIngredients]);
+
+  const filteredIngredientsHandler = (filteredIngredients) => {
+    setUserIngredients(filteredIngredients);
+  };
+
   const addIngredientHandler = async (newIngredient) => {
     //서버: firebase
     const response = await fetch(
@@ -42,7 +80,7 @@ const Ingredients = () => {
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList
           ingredients={userIngredients}
           onRemoveItem={removeIngredientHandler}
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
+  //props 구조분해 할당해서 사용
+  const { onLoadIngredients } = props;
+
+  const [enteredFilter, setEnteredFilter] = useState("");
+
+  const filterHandler = (event) => {
+    setEnteredFilter(event.target.value);
+  };
+
+  //http 요청 보내기
+  useEffect(() => {
+    //사용자가 뭔가 입력할 때 필터링한 데이터를 firebase에서 가져오기
+    //filterHandler 함수로 키 입력이 들어올 때 마다 http 요청 보내면 됨
+    //현재 키 입력이 들어올 때 마다 state 업데이트 하는데, 그 대신 useEffect 사용하여
+    //이펙트 함수 안에서 호출하여 인수로 넣는 함수에서 http 요청 보내기
+
+    const fetchData = async () => {
+      //파이어베이스 데이터 필터링
+      // enteredFilter에 입력된 값이 있으면, title이 enteredFilter와 같은 값 가져와라
+      const query =
+        enteredFilter.length === 0
+          ? ""
+          : `?orderBy="title&equalTo="${enteredFilter}"`;
+
+      const response = await fetch(
+        `https://react-http-35c4a-default-rtdb.firebaseio.com/ingredients.json${query}`
+      );
+
+      const resData = await response.json();
+      const loadedIngredients = [];
+      for (const key in resData) {
+        //loadedIngredients는 상수이지만, push()는 loadedIngredients에 저장된 값을 변경하는게 아닌 메모리에 있는 배열을 변경하는 거기 때문에 사용 가능
+        //세로운 객체 빈 배열인 loadedIngredients에 넣기
+        loadedIngredients.push({
+          id: key,
+          title: resData[key].title,
+          amount: resData[key].amount,
+        });
+      }
+
+      // 데이터 가져오고 나서 검색결과만 보여줘야 하니까 Ingredients 컴포넌트 리스트 거기에 맞게 바꿔줘야함
+      onLoadIngredients(loadedIngredients);
+    };
+
+    fetchData();
+  }, [enteredFilter, onLoadIngredients]); //props.onLoadIngredients 구조분해할당해서 사용해야 의존성에 모든 props가 아닌 onLoadIngredients만 넣억서 원하는 바 대로 작동할 수 있음
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text" value={enteredFilter} onChange={filterHandler} />
         </div>
       </Card>
     </section>
