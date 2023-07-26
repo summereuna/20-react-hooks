d237c1cdb4bb8e5824d6c3d80183384f8811af27
summereuna <summereuna@gmail.com>
2023-07-26 16:12:13 +0900

    Refactor useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -9,34 +9,7 @@ const Ingredients = () => {
   //여기서 재료를 관리한다는건 useState()를 사용해야 한다는 뜻
   const [userIngredients, setUserIngredients] = useState([]);
 
-  //Ingredients 컴포넌트 렌더링 될 때 마다 모든 재료 목록 가져와야 함
-  //컴포넌트가 마운트 될 때 데이터 가져오기
-  //useEffect() 사용
-  useEffect(() => {
-    //Ingredients 컴포넌트가 렌더링된 이후 실행
-    // 그리고 Ingredients 컴포넌트가 렌더링 될 때 마다 실행되는 함수
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://react-http-35c4a-default-rtdb.firebaseio.com/ingredients.json"
-      );
-
-      const resData = await response.json();
-      const loadedIngredients = [];
-      for (const key in resData) {
-        //loadedIngredients는 상수이지만, push()sms loadedIngredients에 저장된 값을 변경하는게 아닌 메모리에 있는 배열을 변경하는 거기 때문에 사용 가능
-        //세로운 객체 빈 배열인 loadedIngredients에 넣기
-        loadedIngredients.push({
-          id: key,
-          title: resData[key].title,
-          amount: resData[key].amount,
-        });
-      }
-
-      setUserIngredients(loadedIngredients);
-    };
-
-    fetchData();
-  }, []);
+  //Ingredients 컴포넌트 렌더링 될 때 마다 모든 재료 목록 가져와야 하는데 이미 Search에서 가져와서 목록에 넣어주고 있기 때문에 두번 중복으로 가져올 필요 없음
 
   useEffect(() => {
     console.log("재료 목록 렌더링: ", userIngredients);
